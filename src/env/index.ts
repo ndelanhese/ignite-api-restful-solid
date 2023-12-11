57a3bca4163f17a996f9465ac89d63e24af7d234
ndelanhese <nnathanh3@gmail.com>
2023-12-11 10:45:58 -0300

    chore(prisma): add prisma

--- src/env/index.ts
+++ src/env/index.ts
@@ -6,6 +6,7 @@ const envSchema = z.object({
   NODE_ENV: z
     .enum(['production', 'development', 'staging', 'test'])
     .default('development'),
+  DATABASE_URL: z.string(),
 })
 
 const _env = envSchema.safeParse(process.env)
