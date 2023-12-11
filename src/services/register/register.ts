ab8044a150d1d583b5327e674ceb79bd381912a0
ndelanhese <nnathanh3@gmail.com>
2023-12-11 14:55:55 -0300

    feat(register): create register repository

--- src/services/register/register.ts
+++ src/services/register/register.ts
@@ -1,6 +1,7 @@
 import { prisma } from '@/lib/prisma'
 import { hash } from 'bcryptjs'
 import { RegisterServiceProps } from './register.types'
+import { PrismaUsersRepository } from '@repositories/prismaUsersRepository'
 
 export const registerService = async ({
   email,
@@ -20,11 +21,11 @@ export const registerService = async ({
     throw new Error('Email already exists!')
   }
 
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash: passwordHash,
-    },
+  const prismaUsersRepository = new PrismaUsersRepository()
+
+  prismaUsersRepository.create({
+    email,
+    name,
+    password_hash: passwordHash,
   })
 }
