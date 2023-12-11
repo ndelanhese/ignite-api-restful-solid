b6b3f8f9135552a76b980d44bf4b9d4335108fb4
ndelanhese <nnathanh3@gmail.com>
2023-12-11 10:07:06 -0300

    chore(alias-path): add alias path

--- src/env/index.ts
+++ src/env/index.ts
@@ -11,9 +11,9 @@ const envSchema = z.object({
 const _env = envSchema.safeParse(process.env)
 
 if (_env.success === false) {
-  console.error('Invalid environment variables!', _env.error.format())
+  console.error('❌ Invalid environment variables!', _env.error.format())
 
-  throw new Error('Invalid environment variables!')
+  throw new Error('❌ Invalid environment variables!')
 }
 
 export const env = _env.data
