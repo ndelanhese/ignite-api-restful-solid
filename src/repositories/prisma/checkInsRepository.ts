ae15d6140196983ce7329db972fd547cde013334
ndelanhese <nnathanh3@gmail.com>
2023-12-13 16:58:16 -0300

    feat(prisma-chec-ins-repository): implemented

--- src/repositories/prisma/checkInsRepository.ts
+++ src/repositories/prisma/checkInsRepository.ts
@@ -1,6 +1,7 @@
 import { prisma } from '@lib/prisma'
 import type { Prisma } from '@prisma/client'
-import { CheckInsRepository } from '../checkInsRepository.types'
+import { CheckIn, CheckInsRepository } from '../checkInsRepository.types'
+import dayjs from 'dayjs'
 
 export class PrismaCheckInsRepository implements CheckInsRepository {
   async create(data: Prisma.CheckInUncheckedCreateInput) {
@@ -10,6 +11,42 @@ export class PrismaCheckInsRepository implements CheckInsRepository {
   }
 
   async findByUserIdOnDate(userId: string, date: Date) {
-    return null
+    const startOfTheDay = dayjs(date).startOf('date')
+    const endOfTheDay = dayjs(date).endOf('date')
+
+    return await prisma.checkIn.findFirst({
+      where: {
+        user_id: userId,
+        created_at: {
+          gte: startOfTheDay.toDate(),
+          lte: endOfTheDay.toDate(),
+        },
+      },
+    })
+  }
+
+  async save(data: CheckIn): Promise<CheckIn> {
+    return await prisma.checkIn.update({
+      where: { id: data.id },
+      data,
+    })
+  }
+
+  async findManyByUserId(userId: string, page = 1) {
+    return await prisma.checkIn.findMany({
+      where: {
+        user_id: userId,
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+  }
+
+  async countByUserId(userId: string) {
+    return await prisma.checkIn.count({ where: { user_id: userId } })
+  }
+
+  async findById(checkInId: string) {
+    return await prisma.checkIn.findUnique({ where: { id: checkInId } })
   }
 }
--- src/repositories/prisma/gymsRepository.ts
+++ src/repositories/prisma/gymsRepository.ts
@@ -1,5 +1,9 @@
 import { prisma } from '@lib/prisma'
-import { Gym, GymsRepository } from '../gymsRepository.types'
+import {
+  Gym,
+  GymsRepository,
+  findManyNearbyProps,
+} from '../gymsRepository.types'
 
 export class PrismaGymsRepository implements GymsRepository {
   async findById(id: string): Promise<Gym | null> {
@@ -14,5 +18,13 @@ export class PrismaGymsRepository implements GymsRepository {
     return await prisma.gym.create({
       data,
     })
+  }
+
+  async searchMany(query: string, page: number): Promise<Gym[]> {
+    throw new Error('Method not implemented.')
+  }
+
+  async findManyNearby(params: findManyNearbyProps): Promise<Gym[]> {
+    throw new Error('Method not implemented.')
   }
 }
