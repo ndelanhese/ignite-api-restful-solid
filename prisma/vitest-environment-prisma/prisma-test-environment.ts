f5bfc54397029940a4200e8d20f3214892e031e7
ndelanhese <nnathanh3@gmail.com>
2023-12-14 11:01:03 -0300

    feat(prisma): add prisma environment to isolate test environment

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,14 +1,38 @@
+import 'dotenv/config'
+
+import { env } from '@env'
+import { randomUUID } from 'node:crypto'
 import { Environment } from 'vitest'
+import { execSync } from 'node:child_process'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const generateDatabaseURL = (schema: string) => {
+  const url = new URL(env.DATABASE_URL)
+  url.searchParams.set('schema', schema)
+  return url.toString()
+}
 
 export default <Environment>{
   name: 'prisma',
   transformMode: 'web',
   async setup() {
-    console.log('setup prisma')
+    const schema = randomUUID()
+
+    const databaseURL = generateDatabaseURL(schema)
+
+    process.env.DATABASE_URL = databaseURL
+
+    execSync('npx prisma migrate deploy')
 
     return {
-      teardown() {
-        console.log('teardown prisma')
+      async teardown() {
+        await prisma.$queryRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+
+        await prisma.$disconnect()
       },
     }
   },
