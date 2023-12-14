f304a49abc1e612e4de2a5da7d6ac36bf0d254fc
ndelanhese <nnathanh3@gmail.com>
2023-12-14 11:13:05 -0300

    feat(authenticate): add e2e test

--- src/http/controllers/authenticate/authenticate.spec.ts
+++ src/http/controllers/authenticate/authenticate.spec.ts
@@ -1,6 +1,34 @@
+import { app } from '@/app'
 import request from 'supertest'
-import { describe } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-describe.skip('Authenticate (e2e)', () => {
-  console.log(request)
+describe('Authenticate (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to authenticate', async () => {
+    const userEmail = 'johndoe@example.com'
+    const userPassword = '123456'
+
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: userEmail,
+      password: userPassword,
+    })
+
+    const response = await request(app.server).post('/sessions').send({
+      email: userEmail,
+      password: userPassword,
+    })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+  })
 })
